aba26f9a526f13f4e5e18219e06398d550800255
Yawmiej <yomlateef@yahoo.com>
2019-11-07 14:04:33 +0100

    Reorganized folder structure and component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, subtitle }) => {
     return (
         <div className="menu-item">
             <div className="content">
-                <h1 className="title">{title}</h1>
-                <span className="subtitle">{subtitle}</span>
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
     )
