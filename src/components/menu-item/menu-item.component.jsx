db0bad906874b2b025dcde59d3c8486e7ffdc1b7
Yawmiej <yomlateef@yahoo.com>
2019-11-07 14:12:12 +0100

    Added menu items images

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, subtitle }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div className="menu-item">
+        <div style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        className={`${size} menu-item`}
+        >
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
